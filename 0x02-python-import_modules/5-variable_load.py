06c539bdd088d51d8a73cd8c24fd118bd8907a16
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-06-08 14:33:09 -0700

    imports a variable from a fle and prints the value

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-from variable_load_5 import a
-def main():
-    print("{}".format(a))
 if __name__ == "__main__":
-    main()
+    from variable_load_5 import a
+
+    print("{}".format(a))
