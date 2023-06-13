264206f8cfaef48f8d9d898e1c1f094a154b912f
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-06-12 17:43:39 -0700

    updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for rows in matrix:
         for items in rows:
-            print("{:d}".format(item), end=" " if items != rows[-1] else "")
+            print("{:d}".format(items), end=" " if items != rows[-1] else "")
         print()
