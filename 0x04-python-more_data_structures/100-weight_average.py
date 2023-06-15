b46079828fb40eb10ca52a62425707989513a1b8
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-06-14 17:32:37 -0700

    returns the weighted average of all integers tuple

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,13 +2,10 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-
     total_score = 0
     total_weight = 0 
-
     for score, weight in my_list:
         total_score += score * weight
         total_weight += weight
-        total_avg = total_score / total_weight
-
+    total_avg = total_score / total_weight
     return (total_avg)
