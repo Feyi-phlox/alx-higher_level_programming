52b68337821ecbc84df9ba932323dfa9ff7ffed3
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-06-26 18:47:16 -0700

    list division

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    try:
+        for i in range(list_length):
+            try:
+                quotient = my_list_1[i] / my_list_2[i]
+                result.append(quotient)
+            except ZeroDivisionError:
+                print("division by 0")
+                result.append(0)
+            except (TypeError, ValueError):
+                print("wrong type")
+                result.append(0)
+            except IndexError:
+                print("out of range")
+                result.append(0)
+    finally:
+        return result
