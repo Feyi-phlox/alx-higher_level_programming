adfe1e5764bb45cdb6826d3f2ae11a011ff9101e
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-06-26 19:10:16 -0700

    safe print list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,11 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for index in my_list[:x]:
-            try:
-                print("{:d}".format(index), end="")
-                count += 1
-            except (TypeError, ValueError):
-                pass
-            print()
-            return count
+    for index in my_list[:x]:
+        try:
+            print("{:d}".format(index), end="")
+            count += 1
+        except (TypeError, ValueError):
+            pass
+        print()
+        return count
