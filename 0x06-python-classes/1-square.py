6381a16b8c0ccc295fa231211e50b75c67bd611a
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-06-28 16:38:52 -0700

    initializes square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" This module defines a Square class."""
+
+
+class Square:
+
+    """defines the function __init__"""
+
+    def __init__(self, size):
+
+        """Initializes a Square instance __size with a given size."""
+        self.__size = size
