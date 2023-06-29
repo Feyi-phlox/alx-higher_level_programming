735da5c265286ca801ff8cfb380abc0ff3958f04
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-06-28 19:23:16 -0700

    compare 2 squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""define class Square"""
+
+
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        """Check if two squares have equal areas."""
+        if isinstance(other, Square):
+            return self.area() == other.area()
+
+    def __ne__(self, other):
+        """Check if two squares have unequal areas."""
+        if isinstance(other, Square):
+            return self.area() != other.area()
+
+    def __gt__(self, other):
+        """Check if one square has a greater area than the other."""
+        if isinstance(other, Square):
+            return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Check if one square has a greater or equal area than the other."""
+        if isinstance(other, Square):
+            return self.area() >= other.area()
+
+    def __lt__(self, other):
+        """Check if one square has a smaller area than the other."""
+        if isinstance(other, Square):
+            return self.area() < other.area()
+
+    def __le__(self, other):
+        """Check if one square has a smaller or equal area than the other."""
+        if isinstance(other, Square):
+            return self.area() <= other.area()
