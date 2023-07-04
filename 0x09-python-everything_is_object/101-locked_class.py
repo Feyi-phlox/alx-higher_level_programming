0ef2515b7be7a33bdb82163a457d2e1f4e53f8b4
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-07-04 08:36:47 -0700

    added

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 class LockedClass:
+    """
+    class that prevents dynamic creation of new instance attribute
+    """
     __slots__ = ['first_name']
 
-    def __setattr__(self, name, value):
+    def __setattr__(self, key, value):
+        """
+        Replaces the default attribute assignment behaviour
+
+        agrs:
+            key(str): name of attribute
+            value(any): value assigned to the attribute
+        raises:
+            AttributeError: if attribute is not 'first_name'
+        """
         if name != 'first_name':
             raise AttributeError("new instance attribute not allowed")
-        object.__setattr__(self, name, value)
+        object.__setattr__(self, key, value)
