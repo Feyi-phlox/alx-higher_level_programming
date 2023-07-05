ce4b9d3c101a827622a41b4e12cc786b08a567a7
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-07-04 18:34:16 -0700

    perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,2 +1,40 @@
 #!/usr/bin/python3
+""" Defines class, Rectangle"""
 
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
+
+    def area(self):
+        return self._width * self._height
+
+    def perimeter(self):
+        if self._width == 0 or self._height == 0:
+            return 0
+        return 2 * (self._width + self._height)
