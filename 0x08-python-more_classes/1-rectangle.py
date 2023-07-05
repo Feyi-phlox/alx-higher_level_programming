81fdb8c3732a22ac498d7dd213f73a5e7e92d967
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-07-04 18:20:06 -0700

    defines a rectangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ Set the width of the rectangle. """
-        if type(value) is not int
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -33,7 +33,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ Set the height of the rectangle. """
-        if type(value) is not int
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
