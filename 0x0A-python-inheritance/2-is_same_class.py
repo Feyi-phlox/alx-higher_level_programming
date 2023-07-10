91a1294d27d325dd80586cb72f01d2101cb8631a
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-07-10 16:13:04 -0700

    checks object instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module reprents the object of a specified class"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Check if an object is exactly an instance of the specified class.
+
+    Parameters:
+    - obj: An object to check.
+    - a_class: The class to compare against.
+
+    Returns:
+    - True if the object is exactly an instance of the specified class; otherwise False.
+
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
