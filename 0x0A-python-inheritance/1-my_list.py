10cb44fa03e82094cbef2c819ddd57561c23f498
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-07-10 16:02:46 -0700

    sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+"""This module represents a class MyList the inherits a base class list"""
+
+
+class MyList(list):
+    """A custom list class that inherits from the built-in list class.
+
+    All elements in the list are assumed to be of type int.
+
+    Args:
+        list (list): The list to initialize the object.
+    """
+
+    def print_sorted(self):
+        """
+        Print the list in ascending order.
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
