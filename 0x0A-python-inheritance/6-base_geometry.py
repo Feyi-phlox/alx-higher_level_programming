8bd9aeb9540e5faf54a9367a608d8577429d890e
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-07-10 16:52:13 -0700

    not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines the basegeometry class"""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
