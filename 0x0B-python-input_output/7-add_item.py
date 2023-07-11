e463303687f84eaca93a61bd690f2ec7b9651e01
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-07-11 06:14:47 -0700

    load, add, save all args of python list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This modules adds all arguments to a Python list and save them to a file"""
 import sys
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -10,5 +9,5 @@ try:
 except FileNotFoundError:
     items = []
 
-items.extend(sys.argv[1:])
+items.append(sys.argv[1:])
 save_to_json_file(items, "add_item.json")
