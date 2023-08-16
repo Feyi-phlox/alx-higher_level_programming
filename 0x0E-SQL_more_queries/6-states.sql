659bf894705f332054c065c8969c1b8056b2f5a7
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-08-16 15:51:17 -0700

    creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- create table in the hbtn_0d_usa database
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO INCREMENT PRIMARY KEY, name VARCHAR (256) NOT NULL);
