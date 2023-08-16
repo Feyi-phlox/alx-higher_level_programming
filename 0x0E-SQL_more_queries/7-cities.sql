0cf48886b2f0611708dcd7dc20172f5c890a5ae8
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-08-16 16:15:58 -0700

    create table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -9,5 +9,5 @@ CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
 
