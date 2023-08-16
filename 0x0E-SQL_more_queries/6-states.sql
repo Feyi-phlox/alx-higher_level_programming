473c42fd45cd87598febba3e6ae9f883f0873073
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-08-16 16:09:05 -0700

    fixed task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,4 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- create table in the hbtn_0d_usa database
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO INCREMENT PRIMARY KEY, name VARCHAR (256) NOT NULL);
+-- creates tables with unique id
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE NOT NULL AUTO INCREMENT PRIMARY KEY,
+	name VARCHAR (256) NOT NULL);
