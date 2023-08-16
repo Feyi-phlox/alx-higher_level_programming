94f6a6804fb430b81e856914863e4e55980e0ff9
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-08-16 16:41:01 -0700

    lists all cities contained in the database hbtn_0d_usa

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- script that lists all cities contained in the database hbtn_0d_usa
+-- Each record should display: cities.id - cities.name - states.name and results must be sorted in ascending order by cities.id
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.state_id = states.id ORDER BY cities.id;
