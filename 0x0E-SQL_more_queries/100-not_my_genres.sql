eaa554108bf31f229b6c1e271cb75aad79c80878
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-08-16 17:26:46 -0700

    list all genres not linked to the show Dexter

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,3 +7,14 @@ WHERE tv_genres.id NOT IN (
       JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
       WHERE tv_shows.title = "DEXTER" )
 ORDER BY tv_genres.name ASC;
+-- or the alias method
+-- SELECT g.name
+-- FROM tv_genres g
+-- WHERE g.name NOT IN (
+--      SELECT g.name
+--      FROM tv_genres g
+--      INNER JOIN tv_show_genres m ON g.id = m.genre_id
+--      INNER JOIN tv_shows s ON m.show_id = s.id
+--       WHERE s.title = 'Dexter'
+-- )
+-- ORDER BY g.name ASC;
