dbfb95f704ef745e90cb691dc6b639230fc486ca
feyi-phlox <feyisayoakinbobola@gmail.com>
2023-09-13 12:12:37 -0700

    a class Square that defines a square and inherits from Square of 5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+class Square extends require('./5-square') {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    const row = c.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
