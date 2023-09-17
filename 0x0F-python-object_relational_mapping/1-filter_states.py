e5e313cefa270376c33634d3e75527b34e735ee0
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-09-17 11:52:41 -0700

    script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This script lists all states with a name
+starting with N from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL:", e)
+        sys.exit(1)
+
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cur.execute(query)
+
+    results = cur.fetchall()
+
+    for row in results:
+        print(row)
+
+    cur.close()
+    db.close()
