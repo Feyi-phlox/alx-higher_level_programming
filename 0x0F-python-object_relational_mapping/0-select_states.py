0b3d359fb02ab94565a6e170399c06a6b59786bb
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-09-17 10:42:53 -0700

    task 0 and readme

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+''' This script lists all states from the database hbtn_0e_0_usa '''
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL:", e)
+        sys.exit(1)
+
+    cur = db.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cur.excute(query)
+
+    results = cur.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
