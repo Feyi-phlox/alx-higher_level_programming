4bd4e585ca17598314f37a2130ca7dcfe0ea125f
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-09-29 11:54:09 -0700

    script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,4 +6,23 @@ from sys import argv
 import requests
 
 
-of __name__ == "__main__":
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+    data = {'q': q}
+
+    try:
+        response = requests.post(url, data=data)
+        content = response.headers.get('content-type')
+
+        if content == 'application/json':
+            jsonres = response.json()
+            if jsonres:
+                print("[{}] {}".format(jsonres['id'], jsonres['name']))
+            else:
+                print("No result")
+    except ValueError:
+        print("Not a valid JSON")
